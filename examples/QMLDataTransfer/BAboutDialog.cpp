553b2e3b267e016418884b642bd0b3ed2055cb8d
周家未 <752736341@qq.com>
2019-12-18 13:17:33 +0800

    remove examples dependency with Builib

---
+++ examples/QMLDataTransfer/BAboutDialog.cpp
@@ -0,0 +1,72 @@
+﻿#include "BAboutDialog.h"
+#include "ui_BAboutDialog.h"
+#include "BUILib.h"
+
+BAboutDialog::BAboutDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::BAboutDialog)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::CustomizeWindowHint);
+    setWindowModality(Qt::WindowModal);
+    setFixedSize(size());
+    ui->buiLogo->setPixmap(QPixmap(":/res/logo/logo.jpg"));
+    connect(ui->okBtn, &QPushButton::clicked, this, &QDialog::close);
+    ui->buiDesc->setText(tr("Support By \nBUI Library Version: %1. \n"
+                            "Qt Framework Version: 5.7.0").arg(BUILib::getVersion()));
+}
+
+BAboutDialog::~BAboutDialog()
+{
+    delete ui;
+}
+
+void BAboutDialog::setLogo(const QPixmap &pix)
+{
+    ui->logo->setPixmap(pix);
+}
+
+void BAboutDialog::setDesc(const QString &desc)
+{
+    ui->programDescLabel->setText(desc);
+}
+
+void BAboutDialog::setOwnership(const QString &desc)
+{
+    ui->ownerLabel->setText(desc);
+}
+
+void BAboutDialog::setRichDetail(const QString &detail)
+{
+    ui->details->insertHtml(detail);
+}
+
+void BAboutDialog::setDetail(const QString &detail)
+{
+    ui->details->setText(detail);
+}
+
+void BAboutDialog::addDetail(const QString &detail)
+{
+    ui->details->append(detail);
+}
+
+void BAboutDialog::setDetailVisible(bool visible)
+{
+    ui->details->setVisible(visible);
+    adjustSize();
+    setFixedSize(size());
+}
+
+void BAboutDialog::setBUIVisible(bool visible)
+{
+    ui->buiLogo->setVisible(visible);
+    ui->buiDesc->setVisible(visible);
+    adjustSize();
+    setFixedSize(size());
+}
+
+void BAboutDialog::retranslateUi()
+{
+    ui->buiDesc->setText(tr("Support By \n%1").arg(BUILib::aboutBuiLib()));
+}
---
+++ examples/QMLDataTransfer/BAboutDialog.h
@@ -0,0 +1,36 @@
+﻿#ifndef BABOUTDIALOG_H
+#define BABOUTDIALOG_H
+
+#include <QDialog>
+#include "builib_global.h"
+
+namespace Ui {
+class BAboutDialog;
+}
+
+class BUILIBSHARED_EXPORT BAboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit BAboutDialog(QWidget *parent = nullptr);
+    ~BAboutDialog();
+
+public slots:
+    void setLogo(const QPixmap &pix);
+    void setDesc(const QString &desc);
+    void setOwnership(const QString &desc);
+
+    void setRichDetail(const QString &detail);
+    void setDetail(const QString &detail);
+    void addDetail(const QString &detail);
+    void setDetailVisible(bool visible);
+
+    void setBUIVisible(bool visible);
+
+    void retranslateUi();
+private:
+    Ui::BAboutDialog *ui;
+};
+
+#endif // BABOUTDIALOG_H
